14cf774c56057dcb3ac01ed7ac24873c5f16e141
Muhammad Salman <154021273+SalmanO7@users.noreply.github.com>
2024-02-09 03:19:33 +0500

    Qort and Auther Typescript

---
+++ famousQorts.ts
@@ -0,0 +1,4 @@
+console.log(`Cristiano Ronaldo said," telent without work is nothing"`);
+//Use Variable
+let auther = "Cristiano Ronaldo"
+console.log(`${auther} said," telent without work is nothing"`)
